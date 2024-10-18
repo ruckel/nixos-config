32ad47b7307ca5358f4781aec8e81227dbf3dfe0
korv <korv@korv.lol>
2024-10-18 03:45:17 +0200

    added dell dwm conf

---
+++ hosts/dell/dwm/bar-functions/dwm_battery.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+bid=BAT0
+
+dwm_battery () {
+# Change BAT1 to whatever your battery is identified as. Typically BAT0 or BAT1
+CHARGE=$(cat /sys/class/power_supply/$bid/capacity)
+STATUS=$(cat /sys/class/power_supply/$bid/status)
+
+statusc='charging'
+statusd=''
+if [ "$IDENTIFIER" = "unicode" ]; then 
+  statusc='🔌 charging'
+  statusd='🔋  battery'
+fi
+
+printf "%s" "$SEP1"
+if [ "$STATUS" = "Charging" ]; then
+  printf "%s%% %s" "$CHARGE" "$statusc"
+else
+    printf "%s%%%s" "$CHARGE" "$statusd"
+fi
+printf "%s\n" "$SEP2"
+}
+
+dwm_battery
+
