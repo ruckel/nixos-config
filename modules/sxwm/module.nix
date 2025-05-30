4c571c76e973ca86841caf61e43e61624b517871
ruckel <kevin.dybeck@gmail.com>
2025-05-30 13:27:56 +0200

    add sxwm

--- modules/sxwm/module.nix
+++ modules/sxwm/module.nix
@@ -1,9 +1,11 @@
 { config, lib, pkgs, ... }:
 # github.com/uint23/sxwm
-{
-  options.sxwm.enable = mkEnableOption "SXWM window manager";
+#with lib;
 
-  config = mkIf config.sxwm.enable {
+{
+  options.sxwm.enable = lib.mkEnableOption "SXWM window manager";
+
+  config = lib.mkIf config.sxwm.enable {
     environment.systemPackages = with pkgs; [
       xorg.libX11
       xorg.libXinerama
