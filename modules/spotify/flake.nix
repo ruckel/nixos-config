ab24f613873b151151cdf101202d6b01ee169ce5
ruckel <kevin.dybeck@gmail.com>
2025-07-28 20:48:24 +0200

    spotify flake

--- modules/spotify/flake.nix
+++ modules/spotify/flake.nix
@@ -1,7 +1,20 @@
 {
   description = "Spotify system module, flake";
 
-  outputs = { self, ... }: {
-    nixosModules.default = import ./module.nix;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosModules = {
+      default = {
+        config = {
+          environment.systemPackages = [ pkgs.spotify ];
+        };
+      };
+    };
   };
 }
