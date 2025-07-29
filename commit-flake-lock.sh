3534dad623dbc98e0f535b8f5cff32bf6aed58dc
ruckel <kevin.dybeck@gmail.com>
2025-07-29 20:39:40 +0200

    added flake.lock .sh script

--- commit-flake-lock.sh
+++ commit-flake-lock.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-git commit flake.lock -m 'chore: commit flake.lock'
+FILEEXISTS=$(git status | grep flake.lock)
+echo "f e : $FILEEXISTS"
+
+#git commit flake.lock -m 'chore: commit flake.lock'
+
