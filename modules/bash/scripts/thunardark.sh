a7d99210783249ecff2723e2a8e882e0ffcbcb1a
ruckel <kevin.dybeck@gmail.com>
2025-08-04 06:55:10 +0200

    bash: thunar update

--- modules/bash/scripts/thunardark.sh
+++ modules/bash/scripts/thunardark.sh
@@ -1,3 +1,9 @@
-#!/bin/sh 
-#exec 
+#!/usr/bin/env bash 
+
+pkill thunar
+#echo "running: env GTK_THEME=Adwaita:dark thunar --daemon"
+env GTK_THEME=Adwaita:dark thunar --daemon &
+
+sleep .1
 env GTK_THEME=Adwaita:dark thunar
+
