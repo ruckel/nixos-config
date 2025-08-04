0f8386097cefbdd044b2a68a2bbc3be9d14e736f
ruckel <kevin.dybeck@gmail.com>
2025-08-04 16:07:04 +0200

    bash: moved script list to external file

--- modules/bash/flake.nix
+++ modules/bash/flake.nix
@@ -8,76 +8,7 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
 
-      scripts = [
-        { name = "cowsay-script";
-          file = scripts/script.sh;
-          deps = with pkgs; [ cowsay ddate ];
-        }
-        { name = "cowsay-scriptus";
-          file = scripts/script2.sh;
-          deps = with pkgs; [ cowsay ];
-        }
-        { name = "exec-with-watch";
-          file = scripts/exec-with-watch.sh;
-          deps = with pkgs; [ inotify-tools ];
-        }
-        { name = "alert";
-          file = scripts/alert.sh;
-          deps = with pkgs; [ toastify mpv ];
-        }
-        { name = "files";
-          file = scripts/thunardark.sh;
-          deps = with pkgs; [ xfce.thunar ];
-        }
-        { name = "backupfile";
-          file = scripts/backupfile.sh;
-          deps = with pkgs; [  ];
-        }
-        #{ name = "";
-        #  file = scripts/x.sh;
-        #  deps = with pkgs; [  ];
-        #}
-        #{ name = "";
-        #  file = scripts/x.sh;
-        #  deps = with pkgs; [  ];
-        #}
-        #{ name = "";
-        #  file = scripts/x.sh;
-        #  deps = with pkgs; [  ];
-        #}
-        #{ name = "";
-        #  file = scripts/x.sh;
-        #  deps = with pkgs; [  ];
-        #}
-        #{ name = "";
-        #  file = scripts/x.sh;
-        #  deps = with pkgs; [  ];
-        #}
-        #{ name = "";
-        #  file = scripts/x.sh;
-        #  deps = with pkgs; [  ];
-        #}
-        #{ name = "";
-        #  file = scripts/x.sh;
-        #  deps = with pkgs; [  ];
-        #}
-        #{ name = "";
-        #  file = scripts/x.sh;
-        #  deps = with pkgs; [  ];
-        #}
-        #{ name = "";
-        #  file = scripts/x.sh;
-        #  deps = with pkgs; [  ];
-        #}
-        #{ name = "";
-        #  file = scripts/x.sh;
-        #  deps = with pkgs; [  ];
-        #}
-        #{ name = "";
-        #  file = scripts/x.sh;
-        #  deps = with pkgs; [  ];
-        #}
-      ];
+      scripts = import ./included-scripts.nix { inherit system; };
 
       makeScript = { name, file, deps }: pkgs.writeShellScriptBin name ''
         export PATH=${pkgs.lib.makeBinPath deps}
---
+++ modules/bash/included-scripts.nix
@@ -0,0 +1,70 @@
+{ pkgs }:[
+  { name = "cowsay-script";
+    file = scripts/script.sh;
+    deps = with pkgs; [ cowsay ddate ];
+  }
+  { name = "cowsay-scriptus";
+    file = scripts/script2.sh;
+    deps = with pkgs; [ cowsay ];
+  }
+  { name = "exec-with-watch";
+    file = scripts/exec-with-watch.sh;
+    deps = with pkgs; [ inotify-tools ];
+  }
+  { name = "alert";
+    file = scripts/alert.sh;
+    deps = with pkgs; [ toastify mpv ];
+  }
+  { name = "files";
+    file = scripts/thunardark.sh;
+    deps = with pkgs; [ xfce.thunar ];
+  }
+  { name = "backupfile";
+    file = scripts/backupfile.sh;
+    deps = with pkgs; [ mpv ];
+  }
+  #{ name = "";
+  #  file = scripts/x.sh;
+  #  deps = with pkgs; [  ];
+  #}
+  #{ name = "";
+  #  file = scripts/x.sh;
+  #  deps = with pkgs; [  ];
+  #}
+  #{ name = "";
+  #  file = scripts/x.sh;
+  #  deps = with pkgs; [  ];
+  #}
+  #{ name = "";
+  #  file = scripts/x.sh;
+  #  deps = with pkgs; [  ];
+  #}
+  #{ name = "";
+  #  file = scripts/x.sh;
+  #  deps = with pkgs; [  ];
+  #}
+  #{ name = "";
+  #  file = scripts/x.sh;
+  #  deps = with pkgs; [  ];
+  #}
+  #{ name = "";
+  #  file = scripts/x.sh;
+  #  deps = with pkgs; [  ];
+  #}
+  #{ name = "";
+  #  file = scripts/x.sh;
+  #  deps = with pkgs; [  ];
+  #}
+  #{ name = "";
+  #  file = scripts/x.sh;
+  #  deps = with pkgs; [  ];
+  #}
+  #{ name = "";
+  #  file = scripts/x.sh;
+  #  deps = with pkgs; [  ];
+  #}
+  #{ name = "";
+  #  file = scripts/x.sh;
+  #  deps = with pkgs; [  ];
+  #}
+]
