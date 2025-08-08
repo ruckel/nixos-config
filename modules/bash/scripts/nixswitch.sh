ccae21dc2c603cbdb260a42b1788b3b9dfa0afd0
ruckel <kevin.dybeck@gmail.com>
2025-08-08 16:48:39 +0200

    nixswitch updated

--- modules/bash/scripts/nixswitch.sh
+++ modules/bash/scripts/nixswitch.sh
@@ -5,14 +5,46 @@ END_COMMENT
 
 version=$(nixos-version | cut -d '.' -f 1,2 | tr -d .)
 
+SUDO="sudo -A "
 basecommand="nixos-rebuild switch --flake ${HOME}/nixos-cfg/# --impure"
 no_reexec=" --no-reexec"
-SUDO="sudo -A "
-Alert="${HOME}/scripts/alert"
+
+
+
+function parseArgs() {
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -h|--help)
+        SHOWHELP=true
+        shift
+        ;;
+      -p|--profile)
+        profile=" --profile $2"
+        shift 2
+        ;;
+      -d|--debug)
+        debug="true"
+        shift 1
+        ;;
+      -*)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+      *)
+        shift
+        ;;
+     esac
+   done
+}
+parseArgs $@
+
 if [[ $version < 2505 ]];then no_reexec=" --fast";fi
-if [[ $HOSTNAME != "nixburk" ]];then Alert='';fi
-final_cmd="${SUDO}${basecommand}${no_reexec}"
+#if [[ $HOSTNAME != "nixburk" ]];then Alert='';fi
+
 
 echo "nixos $version"
+final_cmd="${SUDO}${basecommand}${no_reexec}${profile}"
+
+if [ -n "$debug" ]; then echo "cmd: $final_cmd" && exit; fi
 ${final_cmd}
-$Alert $?
+alert $?
