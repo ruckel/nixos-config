2f79fd58fe2009cee81214a596f1a961120bddf5
ruckel <kevin.dybeck@gmail.com>
2025-08-08 05:53:11 +0200

    vim.nix updated

--- modules/vim.nix
+++ modules/vim.nix
@@ -11,34 +11,15 @@ options.vim = {
 };
 config = mkIf cfg.enable {
   environment.variables = { EDITOR = "vim"; };
-  environment.systemPackages = with pkgs; [((vim_configurable.override {}).customize{
-    name = "vim";
-    vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
-      start = [ vim-nix vim-lastplace ];
-      opt = [];
-    };
-    vimrcConfig.customRC = builtins.readFile ./vimrc;
-    /*vimrcConfig.customRC = ''
-      syntax on
-      set expandtab               "space instead of tabs
-      set shiftwidth=2
-      set tabstop=2
-      set number
-      "set ignorecase
-      set smartcase               "ignores case unless search contains uppercase letters
-
-      map <F10> :set number!<CR>  "! for toggle, nonumber for off
-      map <F9> :set rnu!<CR>
-
-      noremap <leader>y "+y
-      noremap <leader>p "+p
-
-      set foldmethod=indent
-      nnoremap <space> za         " toggle folds with spacebar
-      highlight Folded ctermbg=17
-      highlight Folded ctermfg=14
-     '';
-    */
-  })];
+  environment.systemPackages = with pkgs; [
+    ((vim_configurable.override {}).customize{
+      name = "vim";
+      vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
+        start = [ vim-nix vim-lastplace ];
+        opt = [];
+      };
+      vimrcConfig.customRC = builtins.readFile ./vimrc;
+    })
+  ];
 };
 }
