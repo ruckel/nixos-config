c2e551447ad11df6e1328547757bc08ff4e10935
ruckel <kevin.dybeck@gmail.com>
2025-08-08 05:53:59 +0200

    helper script updated

--- helper-scripts/commit-flake-lock.sh
+++ helper-scripts/commit-flake-lock.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-flakeLock=$(git status | grep -F "flake.lock")
+flakeLock=$(git status | grep -oF "flake.lock")
 #echo "file exists: <$flakeLock>"
 
 if [ -z "$flakeLock" ]; then
   echo "No flake.lock to commit"
   exit 1
 fi
+echo "flakeLock: <${flakeLock}>"
 git add $flakeLock 
 git commit $flakeLock -m 'chore: commit flake.lock'
