0c538183471d5d6061fc6cfc5fc86b59baf33809
ruckel <kevin.dybeck@gmail.com>
2025-08-20 09:48:00 +0200

    tailscale init

--- modules/imports.nix
+++ modules/imports.nix
@@ -30,6 +30,7 @@
     ../modules/ssh.nix
     ../modules/syncthing.nix
     ../modules/systemd.nix
+    ../modules/tailscale.nix
     ../modules/tmux.nix
     ../modules/transmission.nix
     ../modules/vim.nix
