d43915a2c9c9adcc264c5e6319e6a272d81b4f63
ruckel <kevin.dybeck@gmail.com>
2025-08-22 01:28:02 +0200

    mpv syntax fix

--- modules/mpv.nix
+++ modules/mpv.nix
@@ -11,26 +11,22 @@ in {
     ({ # static config
       environment = {
         systemPackages = with pkgs; [ mpv ];
+        shellAliases = { mpv = "mpv --config-dir=/etc/mpv/"; };
         etc = {
           "mpv/input.conf" = { 
             source = ../configfiles/mpv/input.conf;
-            mode = "0444"; 
           };
           "mpv/mpv.conf" = {
             source = ../configfiles/mpv/mpv.conf;
-            mode = "0444"; 
           };
           "mpv/osc_always_on.lua" = {
             source = ../configfiles/mpv/osc_always_on.lua;
-            mode = "0444"; 
           };
           "mpv/cript-opts/osc.conf" = {
             source = ../configfiles/mpv/script-opts/osc.conf;
-            mode = "0444"; 
           };
           "mpv/scripts/toggle_osc_visibility.lua" = {
             source = ../configfiles/mpv/scripts/toggle_osc_visibility.lua;
-            mode = "0444"; 
           };
         };
       };
