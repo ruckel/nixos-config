fbf05932259f92b5337afa0136e157624d271f8f
ruckel <kevin.dybeck@gmail.com>
2025-08-26 01:14:47 +0200

    netbird fix

--- hosts/burk/configuration.nix
+++ hosts/burk/configuration.nix
@@ -6,50 +6,7 @@ let vars = import "${inputs.vars}"; in
     ./hardware-configuration.nix
     ./packages.nix
    ];
-#netbird.client.enable = true;
-/*netbird.clients = {
-  netbird = {
-    port = 51820;
-    hardened = true;
-    interface = "nb0";
-    name = "netbird";
-    #systemd.name = "netbird";
-    #autoStart = true;
-    #bin.suffix = "netbird-default";
-    /*config = {
-      DisableAutoConnect = !client.autoStart;
-      WgIface = client.interface;
-      WgPort = client.port;
-    } // optionalAttrs (client.dns-resolver.address != null) {
-      CustomDNSAddress = "${client.dns-resolver.address}:${builtins.toString client.dns-resolver.port}";
-    }*/
-    #dir.baseName = "netbird-default";
-    #dir.runtime ="/var/run/netbird-default";
-    #dir.state = "/var/lib/netbird-default";
-    #dns-resolver.address
-    #dns-resolver.port
-    /*environment = {
-       NB_STATE_DIR = client.dir.state;
-       NB_CONFIG = "${client.dir.state}/config.json";
-       NB_DAEMON_ADDR = "unix://${client.dir.runtime}/sock";
-       NB_INTERFACE_NAME = client.interface;
-       NB_LOG_FILE = mkOptionDefault "console";
-       NB_LOG_LEVEL = client.logLevel;
-       NB_SERVICE = client.service.name;
-       NB_WIREGUARD_PORT = toString client.port;
-     } // optionalAttrs (client.dns-resolver.address != null) {
-       NB_DNS_RESOLVER_ADDRESS = "${client.dns-resolver.address}:${builtins.toString client.dns-resolver.port}";
-     }
-    */
-    #logLevel = "info"; /* "panic", "fatal", "error", "warn", "warning", ["info"], "debug", "trace" */
-    #openFirewall = true;
-    #service.name = "netbird-default";
-    #suffixedName = "netbird-default";
-    #ui.enable
-    #user.group
-    #user.name*/
-  /*};
-};*/
+netbird.enable = true;};
 services.zoneminder = { enable = false;
   database = {
     createLocally =   true;
