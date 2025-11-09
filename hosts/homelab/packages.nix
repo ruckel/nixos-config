725d891528f033ed59cea6f594187f595d8617ae
ruckel <kevin.dybeck@gmail.com>
2025-11-09 16:13:39 +0100

    homelab pkgs list updated

--- hosts/homelab/packages.nix
+++ hosts/homelab/packages.nix
@@ -1,25 +1,30 @@
-{pkgs, ...}:{ environment.systemPackages = with pkgs; [
-  surf /* browser */
-  cryptsetup
-  deno
-  efibootmgr
-  netbird
-  fastfetch
-  ffmpegthumbnailer
-  fswebcam
-  git
-  gh
-  gitg
-  git-credential-keepassxc
-  keepassxc
-  #mpv
-  mullvad-vpn /* duh */
-  networkmanagerapplet
-  nodejs_20
-  openssl
-  sops
-  sqlite
-  xkbset
-  spotify
-  wget
-];}
+{pkgs, ...}:{
+   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "spotify"
+  ];
+  environment.systemPackages = with pkgs; [
+    surf /* browser */
+    cryptsetup
+    deno
+    efibootmgr
+    netbird
+    fastfetch
+    ffmpegthumbnailer
+    fswebcam
+    git
+    gh
+    gitg
+    git-credential-keepassxc
+    keepassxc
+    #mpv
+    mullvad-vpn /* duh */
+    networkmanagerapplet
+    nodejs_20
+    openssl
+    sops
+    sqlite
+    xkbset
+    spotify
+    wget
+  ];
+}
