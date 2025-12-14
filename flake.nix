6ee37cf4ce8fc13babcbe35069fedee7943b5f07
ruckel <kevin.dybeck@gmail.com>
2025-12-14 03:09:11 +0100

    flake update

--- flake.nix
+++ flake.nix
@@ -2,10 +2,9 @@
 
 inputs = {
   nixpkgs-unstable.url  = "github:NixOS/nixpkgs/nixos-unstable";
-  nixpkgs-stable.url    = "github:NixOS/nixpkgs/nixos-25.05";
+  nixpkgs-stable.url    = "github:NixOS/nixpkgs/nixos-25.11";
   nixpkgs-small.url     = "github:NixOS/nixpkgs/nixos-25.05-small";
   sops-nix.url          = "github:Mic92/sops-nix";
-  bash.url              = "path:./modules/bash";
 };
 
 outputs = {
@@ -14,7 +13,6 @@ outputs = {
   nixpkgs-stable,
   nixpkgs-small,
   sops-nix,
-  bash,
   ...
 } @ inputs: 
 
@@ -25,45 +23,40 @@ in {
   nixosConfigurations = {
     burk  = nixpkgs-unstable.lib.nixosSystem {
       specialArgs = {
-      inherit system inputs outputs vars;
+      inherit system inputs outputs;
         hostName = "burk";
         userName = "korv";
       };
       modules = [
         ./hosts/burk/configuration.nix
         sops-nix.nixosModules.sops
-        bash.nixosModules.default
-        #sxwm.nixosModules.sxwm
-        #spotify.nixosModules.default
       ];
     };
-    dell = nixpkgs-stable.lib.nixosSystem {
+#    dell = nixpkgs-stable.lib.nixosSystem {
+#      specialArgs = {
+#      inherit system inputs outputs;
+#        hostName = "dell";
+#        userName = "korv";
+#      };
+#      modules = [
+#        ./hosts/dell/configuration.nix
+#        sops-nix.nixosModules.sops
+#      ];
+#    };
+    vaio = nixpkgs-small.lib.nixosSystem {
       specialArgs = {
-      inherit system inputs outputs vars;
-        hostName = "dell";
-        userName = "korv";
-      };
-      modules = [
-        ./hosts/dell/configuration.nix
-        sops-nix.nixosModules.sops
-        bash.nixosModules.default
-      ];
-    };
-    /* vaio = nixpkgs-small.lib.nixosSystem {
-      specialArgs = {
-      inherit system inputs outputs vars;
+      inherit system inputs outputs;
         hostName = "vaio";
         userName = "korv";
       };
       modules = [
         ./hosts/vaio/configuration.nix
         sops-nix.nixosModules.sops
-        bash.nixosModules.default
       ];
-      }; */
+     };
     labb = nixpkgs-small.lib.nixosSystem {
       specialArgs = {
-      inherit system inputs outputs vars;
+      inherit system inputs outputs;
         hostName = "labb";
         userName = "user";
 
@@ -71,7 +64,6 @@ in {
       modules = [
         ./hosts/homelab/configuration.nix
         sops-nix.nixosModules.sops
-        bash.nixosModules.default
       ];
     };
   };
